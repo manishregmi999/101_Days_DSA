fcba7ca0589f5e06f3a49a68e083806098fa2629
manishregmi999 <manishregmi007@gmail.com>
2026-01-19 14:40:43 +0545

    Add files via upload

---
+++ Day25.cpp
@@ -0,0 +1,15 @@
+//Infix, Prefix, and Postfix Notations
+	1.	Infix Notation
+		Operator is written between the operands.
+		Example: A + B, (A + B) * C
+     Requires parentheses to indicate order of operations when there are multiple operators.
+
+	2.	Prefix Notation (Polish Notation)
+	Definition: The operator is written before the operands.
+	Example: + A B (equivalent to A + B), * + A B C (equivalent to (A + B) * C)
+	No need for parentheses. order is determined by the position of operators. Useful for computers and stack-based calculations.
+
+	3. Postfix Notation (Reverse Polish Notation)
+	Example: A B + ( A + B), A B + C * (A + B) * C)
+	Does not require parentheses. Easy for computers to evaluate using stacks.
+	The operator is written after the operands.
